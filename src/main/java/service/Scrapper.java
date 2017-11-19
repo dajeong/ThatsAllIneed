1f80d02a9037771db8f0714cb9f8b2b96f00c9f0
Dajeong Kim <galnotepink@gmail.com>
2017-11-20 00:25:58 +0900

    create Scrapper.java and add example url query

---
+++ src/main/java/service/Scrapper.java
@@ -0,0 +1,16 @@
+package service;
+
+public class Scrapper {
+	
+	static final String WEMAKEPRICE = "http://search.wemakeprice.com/search/";
+	static final String WEMAKEPRICE_CATEGORY_DIGITAL = "100000";
+	static final String WEMAKEPRICE_CATEGORY_FOOD_LIVING_BABIES = "100030";
+	static final String WEMAKEPRICE_CATEGORY_FASHION_BEAUTY = "100010";
+	static final String WEMAKEPRICE_CATEGORY_LOCAL_SERVICE = "110100";
+	static final String WEMAKEPRICE_CATEGORY_TOUR_LEISURE = "990000";
+	static final String WEMAKEPRICE_KEYWORKD = "?search_keyword=";
+	static final String WEMAKEPRICE_SORT_CHEAP = "&sort=cheap";
+	static final String WEMAKEPRICE_SORT_FAVORITE = "&sort=favorite";
+	
+	
+}
